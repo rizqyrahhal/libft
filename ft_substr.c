474aa51076d35c45fbefd91cb418e53c56afc65e
rizqyrahhal <rizqyrahhal8@gmail.com>
2021-11-29 14:29:52 +0100

    safi barak

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 06:45:29 by RizQy             #+#    #+#             */
-/*   Updated: 2021/11/29 12:17:39 by rarahhal         ###   ########.fr       */
+/*   Updated: 2021/11/29 14:22:52 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
-	{
-		str = (char *)malloc(sizeof(char));
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
+	if (start >= ft_strlen(s))
+		return(ft_strdup(""));
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	while (s[start] && i < len)
-	{
-		str[i] = s[start];
-		i++;
-		start++;
-	}
+		str[i++] = s[start++];
 	str[i] = '\0';
 	return (str);
 }
